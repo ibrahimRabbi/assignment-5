308ed3dda23d86b467b6ffbfbfa841ed482bda6a
ibrahim rabbi <ibrahimrabbihere@gmail.com>
2023-02-18 22:29:41 +0600

    project done

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function threeInputSumation(btn, input1, input2,name) {
         const value2 = input2.value;
 
         if (value1 < 0 || value2 < 0) {
-          console.log("plz input a posetive number");
+         alert("plz input a posetive number");
         } else if (isNaN(value1) || isNaN(value2)) {
-          console.log("plz input a valid number");
+          alert("plz input a valid number");
         } else if (value1 == "" || value2 == "") {
-          console.log("plz input a number");
+          alert("plz input a number");
         } else {
           const sum = 0.5 * converter(input1) * converter(input2);
           createElement(name, sum);
@@ -33,13 +33,13 @@ function twoInputSumation(btn, input1, input2,name) {
         const value2 = input2.value;
 
         if (value1 < 0 || value2 < 0){
-          console.log("plz input a posetive number");
+           alert("plz input a posetive number");
         }
         else if (isNaN(value1) || isNaN(value2)) {
-          console.log("plz input a valid number");
+           alert(" string is not allowed plz input a valid number");
         }
         else if (value1 == '' || value2 == '') {
-          console.log("plz input a number");
+           alert("plz input a number");
         }
         else {
           const sum = converter(input1) * converter(input2);
@@ -74,52 +74,86 @@ function createElement(name,result) {
     cautch.appendChild(list);
 }
 
+//** this function generate random color *****
+function generateColor(div,apply) {
+  div.addEventListener("mouseover", function () {
+    const randomColor = ["#6495ed", "#e48ae4", "#b815b8", "#f7906b", "#f14f6f"];
+    const generateNumber = Math.floor(Math.random() * randomColor.length);
+    apply.style.backgroundColor = randomColor[generateNumber];
+  })
+}
 
+  
 
-
-
-
-//triangle function call
+//triangle function call and random color function call
 const triangleBtn = document.getElementById("triangle-btn");
 const base = document.getElementById("base-input");
 const height = document.getElementById("height-input");
+
+const triangle = document.getElementById("triangle");
+const applyColor = document.getElementById("apply");
 threeInputSumation(triangleBtn, base, height,"triangle");
+generateColor(triangle,applyColor)
 
 
-
-//rectangle function call
+//rectangle function call and random color function call
 const rectBtn = document.getElementById("rectangle-btn");
 const rectWidth = document.getElementById("rect-base");
 const rectLength = document.getElementById("rect-height");
-twoInputSumation(rectBtn,rectWidth,rectLength,'rectangle')
 
+const rectangle = document.getElementById("rectangle");
+const applyRect = document.getElementById("apply-rect");
+twoInputSumation(rectBtn, rectWidth, rectLength, 'rectangle');
+generateColor(rectangle,applyRect);
 
-//parallelogram eventlistener
+//parallelogram eventlistener and random color function call
 const parallelogramBtn = document.getElementById("parallelogram-btn");
 const paraBase = document.getElementById("para-base")
 const paraHeight = document.getElementById("para-height")
- twoInputSumation(parallelogramBtn,paraBase,paraHeight,'parallelogram')
+
+const parallelogram = document.getElementById("parallelo");
+const applyPara = document.getElementById("apply-para")
+twoInputSumation(parallelogramBtn, paraBase, paraHeight, 'parallelogram');
+generateColor(parallelogram, applyPara);
  
 
- //rhombus function call
+ //rhombus function call and random color function call
 const rhombusBtn = document.getElementById("rhombus-btn");
 const rhombus1 = document.getElementById("rhombus-1");
 const rhombus2 = document.getElementById("rhombus-2");
-threeInputSumation(rhombusBtn, rhombus1, rhombus2,"rhombus");
 
-//pentagone function call 
+const rhombus = document.getElementById("rhombus")
+const applyRhombus = document.getElementById("apply-rhombus");
+
+threeInputSumation(rhombusBtn, rhombus1, rhombus2,"rhombus");
+generateColor(rhombus, applyRhombus)
+
+//pentagone function call and random color function call 
 const pentagoneBtn = document.getElementById("pentagone-btn");
 const pentagoneP = document.getElementById("pentagone-p");
 const pentagoneB = document.getElementById("pentagone-b");
-threeInputSumation(pentagoneBtn, pentagoneP, pentagoneB,'pentagone');
 
-//ellipse function call 
+const pentagone = document.getElementById("pentagone");
+const applyPentagone = document.getElementById("apply-pentagone")
+
+threeInputSumation(pentagoneBtn, pentagoneP, pentagoneB,'pentagone');
+generateColor(pentagone, applyPentagone)
+
+//ellipse function call and random color function call 
 const ellipseBtn = document.getElementById("ellipse-btn");
 const ellipse1 = document.getElementById('ellipse-1')
 const ellipse2 = document.getElementById('ellipse-2')
+
+const ellipse = document.getElementById("ellipse")
+const applyEllipse = document.getElementById("apply-ellipse")
+
 ellipseBtn.addEventListener('click', function () {
-    const pi = Math.PI;
-    const sum = converter(ellipse1) * converter(ellipse2) * pi
-    alert(sum)
-    createElement('ellipase')
-})
+  const pi = Math.PI;
+  const sum = converter(ellipse1) * converter(ellipse2) * pi
+  alert(sum)
+  createElement('ellipase')
+});
+generateColor(ellipse,applyEllipse)
+
+
+
